5fad13d71efc9b1141c00a6d1e03519776abb06c
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-11-01 17:25:27 +0530

    MT

--- src/main/java/net/flood/ocrnn/TaskListener.java
+++
@@ -1,8 +0,0 @@
-package net.flood.ocrnn;
-
-/**
- * @author flood2d
- */
-public interface TaskListener<T> {
-    void onTaskEnd(T result);
-}
--- src/main/java/net/flood/ocrnn/ui/Animation.java
+++
@@ -1,9 +0,0 @@
-package net.flood.ocrnn.ui;
-
-/**
- * @author flood2d
- */
-public interface Animation {
-
-    void animate(long deltaMs);
-}
